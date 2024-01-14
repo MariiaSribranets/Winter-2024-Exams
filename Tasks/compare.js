a602a83b947eb1d13fc51c1396586005c1663f7c
saoriyll <mmmsribranets@gmail.com>
2024-01-14 17:42:53 +0200

    Added const and empty lines. Changed quotes

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,13 +1,16 @@
 // Compare two dictionaries
 
-let compare = (first_values, ...parameters_LIST) => {
-  const second_values = parameters_LIST[0];
-  let firstKeys = Object.keys(first_values);
-  let secondKeys = Object.keys(second_values);
-  if (firstKeys.join('-') !== secondKeys.join('-')) return false;
-  for (key of firstKeys) {
+let compare = (first_values, second_values) => {
+
+  const firstKeys = Object.keys(first_values);
+  const secondKeys = Object.keys(second_values);
+
+  if (firstKeys.join("-") !== secondKeys.join("-")) return false;
+
+  for (const key of firstKeys) {
     if (first_values[key] === second_values[key]) false;
   }
+
   return true;
 };
 
