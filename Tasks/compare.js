ff4b8dcb572d4e5f3566cadcdea7550286c89dd4
saoriyll <mmmsribranets@gmail.com>
2024-01-14 17:29:33 +0200

    Removed unneeded variable and else statement

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -5,14 +5,10 @@ let compare = (first_values, ...parameters_LIST) => {
   let a = Object.keys(first_values);
   let b = Object.keys(second_values);
   if (a.join('-') !== b.join('-')) return false;
-  let e = true;
   for (c of a) {
     if (first_values[c] === second_values[c]) e = e && true;
-    else {
-      e = e && false;
-    }
   }
-  return e;
+  return true;
 };
 
 module.exports = compare;
