5493999a7f701f540d6e7c18334e041f48a3564d
saoriyll <mmmsribranets@gmail.com>
2024-01-14 19:46:20 +0200

    Optimized function

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,15 +2,7 @@
 'use strict';
 
 const filter = (array, type) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
-    }
-  }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  return array.filter((item) => typeof item === type);
 };
 
 module.exports = filter;
