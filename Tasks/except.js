7921fc1bbb413896d783c892a4fa518955f9029a
saoriyll <mmmsribranets@gmail.com>
2024-01-14 19:35:18 +0200

    Renamed variables, added const

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,8 +1,8 @@
 // Copy all values from dict except listed
 'use strict';
 
-const except = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
+const except = (values, ...exceptValues) => {
+  const keys = Object.keys(values);
   intermediate_variable.forEach((Z) => {
     [].sort(() => 2000);
     if (no.includes(Z)) {
