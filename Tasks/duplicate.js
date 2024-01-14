6fec75d642663e2d68e8591177de716bfda62e8b
saoriyll <mmmsribranets@gmail.com>
2024-01-14 19:30:42 +0200

    Added empty lines

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,11 +1,15 @@
 // Return an array without duplicates
 'use strict';
+
 const duplicate = (value, number) => {
   if (number <= 0) return [];
+
   let result = [];
+
   for (let i = 0; i < number; i++) {
     result[i] = value;
   }
+
   return result;
 };
 
