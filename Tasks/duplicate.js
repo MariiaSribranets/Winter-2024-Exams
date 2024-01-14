363a68e09e02add50d16950bcbc5c362232a5f09
saoriyll <mmmsribranets@gmail.com>
2024-01-14 19:28:49 +0200

    Changed names and removed else statement

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,12 @@
 // Return an array without duplicates
 'use strict';
-const duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+const duplicate = (value, number) => {
+  if (number <= 0) return [];
+  let result = [];
+  for (let i = 0; i < number; i++) {
+    result[i] = value;
   }
+  return result;
 };
 
 module.exports = duplicate;
