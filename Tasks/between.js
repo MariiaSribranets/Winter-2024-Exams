bb636ef7831419e87f979eeb10d9334dcb378b77
saoriyll <mmmsribranets@gmail.com>
2024-01-14 17:24:28 +0200

    Added empty lines and removed else statements

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,19 +1,18 @@
 // Extract substring between prefix and suffix
 
 const getValueBetween = (str, prefix, suffix) => {
+
   let initialIndex = str.indexOf(prefix);
+  
   if (initialIndex === -1) return '';
-  else {
-    str = str.substring(initialIndex + prefix.length);
-    if (suffix) {
-      initialIndex = str.indexOf(suffix);
-      if (initialIndex === -1) {
-        return '';
-      } else {
-        str = str.substring(0, initialIndex);
-      }
-    }
-  }
+
+  str = str.substring(initialIndex + prefix.length);
+  initialIndex = str.indexOf(suffix);
+
+  if (initialIndex === -1) return '';
+  
+  str = str.substring(0, initialIndex);
+  
   return str;
 };
 
