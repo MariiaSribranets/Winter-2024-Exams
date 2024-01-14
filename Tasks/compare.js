b53ea029c78de70b5123da24ef69ac730ca66b65
saoriyll <mmmsribranets@gmail.com>
2024-01-14 17:32:47 +0200

    Changed names and removed unneeded elements

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -2,11 +2,11 @@
 
 let compare = (first_values, ...parameters_LIST) => {
   const second_values = parameters_LIST[0];
-  let a = Object.keys(first_values);
-  let b = Object.keys(second_values);
-  if (a.join('-') !== b.join('-')) return false;
-  for (c of a) {
-    if (first_values[c] === second_values[c]) e = e && true;
+  let firstKeys = Object.keys(first_values);
+  let secondKeys = Object.keys(second_values);
+  if (firstKeys.join('-') !== secondKeys.join('-')) return false;
+  for (key of firstKeys) {
+    if (first_values[key] === second_values[key]) false;
   }
   return true;
 };
