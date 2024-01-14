74217f9b413d9c1b9e7512166a6b9b401224222e
saoriyll <mmmsribranets@gmail.com>
2024-01-14 17:43:13 +0200

    Changed quotes

--- Tasks/between.js
+++ Tasks/between.js
@@ -4,12 +4,12 @@ const getValueBetween = (str, prefix, suffix) => {
 
   let initialIndex = str.indexOf(prefix);
   
-  if (initialIndex === -1) return '';
+  if (initialIndex === -1) return "";
 
   str = str.substring(initialIndex + prefix.length);
   initialIndex = str.indexOf(suffix);
 
-  if (initialIndex === -1) return '';
+  if (initialIndex === -1) return "";
   
   str = str.substring(0, initialIndex);
   
